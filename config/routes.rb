a6bb74ffbb4d3ee827c4ebab62e905bb0c721bae
mtbsickrider <enriquepadilla@outlook.com>
2015-03-01 16:51:23 -0800

    Finished Login-Logout, missing tracking user for browser close

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'welcome/index'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
